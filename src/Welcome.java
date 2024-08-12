93751f75c335e018d041c42c25f2afbcd7255274
LeVanLyHub <23737351.ly@student.iuh.edu.vn>
2024-08-12 22:20:29 +0700

    message

---
+++ src/Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Le Van Ly!");
+        System.out.println("Welcome to the DHKTPM19B!");
+    }
+}
